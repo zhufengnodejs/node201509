5aed63170463b404e01c9f3ca8ec9877a1a120aa
zfpx <zfpx@126.com>
2015-12-26 16:54:44 +0800

    global process

--- 4.global/7.util.js
+++ 4.global/7.util.js
@@ -2,6 +2,7 @@
  * util
  *
  **/
+var util = require('util');
 function Parent(){
     this.name = 'Parent';
     this.age = 6;
@@ -17,9 +18,46 @@ function Child(){
     this.name = 'Child';
 }
 //不能传参 会继承私有属性
-Child.prototype = new Parent();
+//Child.prototype = new Parent(); //Parent.prototype
+//Child.prototype = Object.create(Parent.prototype);
+util.inherits(Child,Parent);
 
 var child = new Child();
 //console.log();
 child.showName();
-console.log(child. === Object.prototype);
+// Object Parent Child
+console.log(child.__proto__.__proto__.__proto__ == Object.prototype
+);
+function Person(){
+    this.name = 'zfpx';
+    this.parent = {
+        name:'parent'
+    }
+}
+Person.prototype.toString = function(){
+    console.log('this is ',this.name);
+}
+
+var p = new Person();
+p.toString();
+/**
+ * showHidden 是否显示隐藏属性
+ * depth 对象的递归显示深度
+ * colors 是否显示 颜色
+ */
+console.log(util.inspect(p,true,1,true));
+
+var arr1 = [1,2];
+var arr2 = [3,4];
+//console.log(arr1.concat(arr2));
+Array.prototype.push.apply(arr1,arr2);
+arr1.push(3);
+arr1.push(4);
+console.log(arr1);
+
+
+console.log(util.isArray([]));
+console.log(util.isDate([]));
+console.log(util.isRegExp([]));
+
+
