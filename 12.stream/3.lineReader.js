84fa9ec794120abb83c0abc5611e453fca8ad906
zfpx <zfpx@126.com>
2016-01-09 12:12:33 +0800

    行读取器

--- 12.stream/3.lineReader.js
+++ 12.stream/3.lineReader.js
@@ -1,16 +1,23 @@
 var fs = require('fs');
-var rs  = fs.createReadStream('./read.txt',{
-    start:0,end:1,highWaterMark:2
-})
-rs.on('readable',function(){
-    console.log('=====readable=====');
-    var buff;
-    while(null != (buff = rs.read(1))){
-      var char =   buff[0];
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+///r/n 0x0d 0x0a
+var RETURN= 0x0d; // ascii \r       十六进制的d就等于十进制的13  回车 return 移动光标到该行的起始位置
+var NEWLINE = 0x0a; // ascii  \n   十六进制的a等于十进制的10 换行 newline 换行至下一行行首起始位置
 
-    }
+function LineReader(path){
+    this._rs = fs.createReadStream(path,'r');
+}
+util.inherits(LineReader,EventEmitter);
+
+LineReader.prototype.on('newListener',function(name,func){
+
 });
 
-rs.on('end',function(){
-
+var reader = new LineReader('./1.txt');
+reader.on('newLine',function(data){
+    console.log(data);
+});
+reader.on('end',function(){
+    console.log('end');
 });
