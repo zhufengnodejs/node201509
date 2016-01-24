d315b2acf967f12d99acf1b3e82e675873f837bd
zfpx <zfpx@126.com>
2016-01-24 13:33:47 +0800

    解决jsonpbug

---
+++ 23.angular/4.自定义指令/jsonp.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+app.use(function(req,res,next){
+    console.log(req.url);
+    next();
+});
+app.all('/check',function(req,res){
+  var data = req.query;
+    console.log(data);
+    var callback = data.callback;
+    if(data.username == 'admin'){
+        res.send(callback+'({"unique":false})');
+    }else{
+        res.send(callback+'({"unique":true})');
+    }
+});
+app.use(function(req,res,next){
+    res.end('data');
+});
+
+app.listen(8080);
